38ac295e8e2b432fb1d4a525e234a9a99a525e93
ts1163 <hwkim@tidesquare.com>
2023-02-14 18:12:36 +0900

    🚧 digit 입력 처리 코드 작성 중

--- hw-calculator/src/App.jsx
+++ hw-calculator/src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react';
-import Digit from './components/Digit';
-import Modifier from './components/Modifier';
-import Operation from './components/Operation';
+import { useEffect, useState } from "react";
+import Digit from "./components/Digit";
+import Modifier from "./components/Modifier";
+import Operation from "./components/Operation";
 
 export default function App() {
-  const [firstNumber, setfirstNumber] = useState(0);
+  const [firstNumber, setFirstNumber] = useState(0);
   const [secondNumber, setSecondNumber] = useState(0);
   const [inputValue, setInputValue] = useState(0);
+  const [clickedValue, setClickedValue] = useState(0);
+  const [isLimit, setIsLimit] = useState(false);
+
+  useEffect(() => {
+    if (isLimit) alert("3자리 수 까지만 입력가능.");
+    else {
+      setInputValue((inputValue) => inputValue * 10 + clickedValue);
+      setIsLimit((islLimit) => inputValue >= 1000);
+    }
+  }, [clickedValue]);
 
   return (
     <div className="App">
       <div className="calculator">
         <h1 id="total">{inputValue}</h1>
-        <Digit maxDigit="9" />
+        <Digit maxDigit="9" onHandleClick={setClickedValue} flag={isLimit} />
         <Modifier role="AC" />
         <Operation />
       </div>
--- hw-calculator/src/components/Digit.jsx
+++ hw-calculator/src/components/Digit.jsx
@@ -1,10 +1,19 @@
-import Button from './common/Button';
+import Button from "./common/Button";
 
 export default function Digit(props) {
   const digitArr = [];
   for (let digit = 0; digit <= props.maxDigit; digit++) digitArr.unshift(digit);
+
+  function updateClickedValue(clickedValue) {
+    if (isNaN(clickedValue) || props.flag) return false;
+    props.onHandleClick(clickedValue);
+  }
+
   return (
-    <div className="digits flex">
+    <div
+      className="digits flex"
+      onClick={(evt) => updateClickedValue(Number(evt.target.innerText))}
+    >
       {digitArr.map((digit) => (
         <Button key={digit} style="digit" value={digit} />
       ))}
